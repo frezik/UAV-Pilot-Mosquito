83ccdd77fa45e64eb29fbc8c490a417f03524f10
tmurray <tmurray@wumpus-cave.net>
2014-05-11 17:37:55 -0500

    Initial commit

---
+++ lib/UAV/Pilot/Mosquito.pm
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+package UAV::Pilot::Mosquito;
+
+# ABSTRACT: Control small 2.4GHz UAVs
+
+1;
+
+
+=head1 NAME
+
+  UAV::Pilot::Mosquito - Control small 2.4GHz UAVs
+
+=cut
